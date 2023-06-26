b76453b797e2319a5cdfbb7c8c36f02e8c0a57dd
gupta.kundan <gupta.kundan@thinksys.com>
2023-06-26 17:49:26 +0530

    fake-api

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Fakeapi from './component/Fakeapi';
+
+function App() {
+
+  return (
+    <div className="App">
+        <h1> Hello World </h1>
+        <Fakeapi />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Fakeapi.js
@@ -0,0 +1,96 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios';
+
+
+const API = 'https://fakestoreapi.com'
+
+const Fakeapi = () => {    
+    const [productsList, setProductsList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [productsCategories, setProductsCategories] = useState([])
+    const [allproducts, setAllproducts] = useState([]);
+    const [active, setActive] = useState();
+
+    const getApiData = (url) => {
+      try {
+        axios.get(url)
+          .then((response) =>{
+            setProductsList(response.data)
+            setAllproducts(response.data)
+            setIsLoading(false)            
+          })                
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    const getProductsCategories = (url) => {
+      try {
+        axios.get(url)
+          .then((response) =>{
+            const data = response.data
+            const newData = ['All', ...data];
+            setProductsCategories(newData)
+          })       
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+
+    useEffect(() => {
+      getApiData(API+'/products');
+      getProductsCategories(API+'/products/categories');
+    }, [])
+
+    const filterCategories = (categoriesItem) => {
+      setActive(categoriesItem);
+      if(categoriesItem == 'All'){
+        return setProductsList(allproducts)
+      }
+      const updatedItem = allproducts.filter( (currentItem) => {
+          return currentItem.category == categoriesItem;
+      })
+      setProductsList(updatedItem)
+    }
+
+
+  return (
+    <>
+
+    <div className='products-categories'>
+    <ul>
+        {productsCategories.map((categories, index) => {
+          return (
+              <li key={index} className={active === categories ? "active" : "inactive"}  onClick={ ()=> {
+                filterCategories(categories)
+              }}>{categories}</li>
+          )
+        })} 
+        </ul>
+    </div>
+
+
+      <div className='box-wrapper'>
+        {isLoading ? <h3>Loading data...</h3> : productsList.map((products) =>{
+            const {id, category, description, image, price, title} = products;
+            return (
+                <div className='box' key={id}>
+                    <h2>{category}</h2>
+                    <p className='description'>{description}</p>
+                    <div>
+                        <img src={image} alt={image} />
+                    </div>
+                    <p>{price}</p>
+                    <p>{title}</p>
+
+                    <button onClick={ ()=> {
+                      // addTocart()
+                    }}>Add to Cart</button>
+                </div>
+            )
+        })}
+        </div>
+    </>
+  )
+}
+
+export default Fakeapi;
